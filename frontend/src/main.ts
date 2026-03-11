cd89f6e3e19b119219445d6121e4f27e7f67481e
Julius Boateng <juliusopokuboateng@gmail.com>
2026-03-10 20:45:58 -0500

    Add more methods to PuzzleSession

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -94,7 +94,10 @@ class PuzzleController {
     }
 
     private handleLetter(key: string) {
-        this.session.setLetter()
+        // const value = input.value;
+        // this.setLetter(value === "" ? null : value);
+
+        // this.session.setLetter()
     }
 
     private handleArrowKey() {
@@ -106,16 +109,19 @@ class PuzzleController {
     }
 }
 
+type PlacementId = number;
+type WordLength = number;
+
 class PuzzleSession {
-    row: number
-    col: number
-    rows: number
-    cols: number
-    activePlacement: Placement
+    row: number;
+    col: number;
+    rows: number;
+    cols: number;
+    activePlacement: Placement;
     activePlacementIndex: number;
-    // currentWordLength: Map<Placement, number>,
-    letterGrid: (string | null)[][]
-    boardView: BoardView
+    currentWordLength: Map<PlacementId, WordLength>;
+    letterGrid: (string | null)[][];
+    boardView: BoardView;
 
 
     constructor(boardView: BoardView) {
@@ -124,23 +130,24 @@ class PuzzleSession {
         this.cols = this.boardView.board.cols;
 
         // start cursor on first placement
-        this.activePlacement = this.boardView.getPlacements()[0];
+        this.activePlacement = this.getInitialPlacement();
         this.activePlacementIndex = 0;
         this.row = this.activePlacement.start_row;
         this.col = this.activePlacement.start_col;
-        // this.currentWordLength = 
+        this.currentWordLength = this.createWordLengthMap();
 
         this.letterGrid = this.createLetterGrid();
     }
+    
     advanceCursor() {
-        const next = this.getNextCell();
+        const next = this.getCellInActiveWord(1);
         if (!next) return;
     
         this.moveCursor(next.row, next.col);
     }
 
     reverseCursor() {
-        const previous = this.getPreviousCell();
+        const previous = this.getCellInActiveWord(-1);
         if (!previous) return;
     
         this.moveCursor(previous.row, previous.col);
@@ -189,53 +196,105 @@ class PuzzleSession {
 
     setLetter(letter: string | null) {
         if (this.isBlock()) {
-            throw Error("Unable to write to block cell.")
+            throw Error("Unable to write to block cell.");
         }
-        
-        if (letter !== null) {
-            if (letter.length !== 1) {
-                throw new Error("Letter must be a single character.");
-            }
-            letter = letter.toUpperCase();
-        }
-
-        this.letterGrid[this.row][this.col] = letter
+    
+        letter = this.validateLetter(letter);
+        const prev = this.letterGrid[this.row][this.col];
+    
+        if (prev === letter) return;
+    
+        this.letterGrid[this.row][this.col] = letter;
+    
+        if (prev === null && letter !== null) this.adjustWordLengths(1);
+        if (prev !== null && letter === null) this.adjustWordLengths(-1);
     }
 
     getLetter(): string | null {
         return this.letterGrid[this.row][this.col]
     }
 
-    // isWordComplete(): boolean {
-    //     const placement = this.activePlacement;
+    isWordComplete(): boolean {
+        const placement = this.activePlacement;
+        const length = this.currentWordLength.get(placement.id) ?? -1
+        return length == placement.length
+    }
 
-    // }
+    private getInitialPlacement(): Placement {
+        const placements = this.boardView.getPlacements();
+        if (placements.length === 0) {
+            throw new Error("Puzzle contains no placements.");
+        }
 
+        return placements[0];
+    }
+
+    // Regex expression that checks if a character is a Unicode or Arabic character
+    private isLetterOrDigit(char: string): boolean {
+        return /[\p{L}\p{Nd}]/u.test(char);
+    }
+
+    private validateLetter(letter: string | null): string | null {
+        if (letter === null) return null;
+
+        if (letter.length !== 1) {
+            throw new Error("Letter must be a single character.");
+        }
+
+        const char = letter.toUpperCase();
+
+        if (!this.isLetterOrDigit(char)) {
+            throw new Error("Invalid character.");
+        }
+
+        return char;
+    }
+
+    private adjustWordLengths(delta: number) {
+        const cell = this.boardView.getCell(this.row, this.col);
+        if (!cell) return;
+    
+        const positions = cell.placement_positions;
+    
+        if (positions.A) {
+            const id = positions.A.placement_id;
+            const value = this.currentWordLength.get(id) ?? 0;
+            this.currentWordLength.set(id, value + delta);
+        }
+    
+        if (positions.D) {
+            const id = positions.D.placement_id;
+            const value = this.currentWordLength.get(id) ?? 0;
+            this.currentWordLength.set(id, value + delta);
+        }
+    }
+    
     private createLetterGrid() {
         const letterGrid = Array.from({length: this.rows}, () => Array(this.cols).fill(null))
         return letterGrid;
     }
 
-    private getNextCell(): {row: number; col: number} | null {
-        const nextIndex = this.activePlacementIndex + 1;
-        if (nextIndex >= this.activePlacement.length) return null;
+    private createWordLengthMap(): Map<PlacementId, WordLength> {
+        const map = new Map();
+        const placements = this.boardView.getPlacements()
+        for (const placement of placements) {
+            map.set(placement.id, 0);
+        }
     
-        const cells = this.boardView.getCellsWithPlacementId(this.activePlacement.id);
-        if (!cells) return null;
-
-        const nextCell = cells[nextIndex]; // cells are sorted
-        return {row: nextCell.row, col: nextCell.col};
+        return map;
     }
 
-    private getPreviousCell(): {row: number; col: number} | null {
-        const previousIndex = this.activePlacementIndex - 1;
-        if (previousIndex < 0) return null;
+    // positive offset provides next cell in placement. negative offset provides previous cell.
+    private getCellInActiveWord(offset: number): {row: number; col: number} | null {
+        const relativeIndex = this.activePlacementIndex + offset;
+    
+        if (relativeIndex < 0 || relativeIndex >= this.activePlacement.length) return null;
     
         const cells = this.boardView.getCellsWithPlacementId(this.activePlacement.id);
         if (!cells) return null;
-
-        const previousCell = cells[previousIndex]; // cells are sorted
-        return {row: previousCell.row, col: previousCell.col};
+    
+        const cell = cells[relativeIndex]; // cells are sorted
+        return {row: cell.row, col: cell.col};
     }
 
     private isBlock(): boolean {
