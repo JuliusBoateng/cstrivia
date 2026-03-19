5fc5671254654dde5b4b79fbb38a69f73237170a
Julius Boateng <juliusopokuboateng@gmail.com>
2026-03-19 04:54:57 -0500

    Move index.html to crossword.html. Create new index.html

--- crossword/views.py
+++ crossword/views.py
@@ -4,7 +4,11 @@ from .service import get_views
 
 # Create your views here.
 def index(request):
+    return render(request, "crossword/index.html")
+
+def crossword(request):
+    print("hmmm")
     views = get_views(3)
     data = {"board_view_dto": views.serialized_board_view,
             "solution_view_dto": views.serialized_solution_view}
-    return render(request, "crossword/index.html", data)
+    return render(request, "crossword/crossword.html", data)
